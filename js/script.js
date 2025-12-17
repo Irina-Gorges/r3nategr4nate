d4526412ae1b56cd410343077bc69302851913cb
Irina Gorges <r3nategr4nate@gmail.com>
2025-12-17 11:04:37 +0100

    added text in hero section

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*====================== typed js ======================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Softwareentwicklerin', 'Webentwicklerin', 'Web Designerin'],
+    strings: ['Twitch Streamerin', 'Vollblutschwäbin', 'Gamerin'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
